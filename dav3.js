43c9e877560a1dd178738afce5de855d26b76caf
nsher888 <51024662+nsher888@users.noreply.github.com>
2021-11-29 15:33:28 +0400

    dav 3

---
+++ dav3.js
@@ -0,0 +1,15 @@
+function complex(...args) {
+    try {
+        const checkNum = args.filter((item) => typeof item === "number");
+        const checkTimes = checkNum.map((item) => item % 2 === 0 ? item / 2 : item ** 2);
+        const sum = checkTimes.reduce((a, b) => a + b);
+        return Math.floor(sum / 2);
+    }
+    catch {
+        const error = "No numbers in array";
+        return error;
+    }
+}
+
+console.log(complex("Moon", 2, 3, null, true, 4, 5, 23.3));
+console.log(complex('name', false));
