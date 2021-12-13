5163a0131c90b250a49c73e802868d6679c53a5e
nsher888 <51024662+nsher888@users.noreply.github.com>
2021-12-13 18:49:13 +0400

    dav7-2

---
+++ dav7-2/app.js
@@ -0,0 +1,34 @@
+const url = 'https://api.quotable.io/random';
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const btn = document.querySelector('.btn');
+
+
+
+
+
+// axios.get(url)
+//     .then(response => {
+//         console.log(response);
+//     })
+
+
+
+
+async function quoteGenerator() {
+    try {
+        const response = await axios.get(url)
+        quote.innerText = response.data.content;
+        author.innerText = response.data.author;
+
+    } catch (error) {
+        const text = document.body;
+        const p = document.createElement('p');
+        p.textContent = error;
+        text.appendChild(p);
+        return error;
+    }
+}
+
+
+btn.addEventListener('click', quoteGenerator)
